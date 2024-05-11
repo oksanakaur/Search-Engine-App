87de235cf8a8b9b51239c6992b70410c09b124fd
oksanakaur <160805189+oksanakaur@users.noreply.github.com>
2024-05-11 09:07:55 +0200

    Injected javascript for the icon

--- src/index.js
+++ src/index.js
@@ -7,8 +7,9 @@ function refreshWeather(response) {
     let windSpeedElement = document.querySelector("#wind-speed");
     let timeElement = document.querySelector("#time");
     let date = new Date(response.data.time * 1000);
+    let iconElement = document.querySelector("#icon");
 
-
+    iconElement.innerHTML =`<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
     cityElement.innerHTML = response.data.city;
     timeElement.innerHTML = formatDate(date);
     descriptionElement.innerHTML = response.data.condition.description;
