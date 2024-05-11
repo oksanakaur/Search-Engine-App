dad382ab2397320cecbc842f5a19b2bd56458032
oksanakaur <160805189+oksanakaur@users.noreply.github.com>
2024-05-11 06:21:38 +0200

    Added basic search engine

---
+++ src/index.js
@@ -0,0 +1,10 @@
+function handleSearchSubmit(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = searchInput.value;
+}
+
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
